c7740e09dec3cbd335b78abdd8baaf5b487194ef
emilio_amaya <eamayaf@udistrital.edu.co>
2025-06-29 16:30:01 -0500

    Creacion Inicial Interfazes visuales

---
+++ src/MenuInicial.java
@@ -0,0 +1,11 @@
+ import javax.swing.*;
+
+public class MenuInicial {
+    public JButton iniciarSesiónButton;
+    public JButton registrarseButton;
+
+public MenuInicial(){
+    iniciarSesiónButton.addActionListener(e -> )
+    registrarseButton.addActionListener(e -> )
+}
+}
